6429d967a1cef05d7be2977f6316175387e75f85
Muhamad Iqbal Nurzaman <iqbal.nurzaman@andalin.com>
2023-07-18 16:13:40 +0700

    Third commit

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
             <div>
               <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Muhamad Iqbal Nurzaman</h1>
               <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Senior Backend Engineer</h2>
-              <p className="mt-4 max-w-xs leading-normal">Ready to create your dream Web Services</p>
+              <p className="mt-4 max-w-xs leading-normal typewriter">Ready to create your dream Web Services</p>
               <nav className="nav hidden lg:block">
                 <ul className="mt-16 w-max">
                   <li>
@@ -63,12 +63,12 @@ function App() {
             </div>
             <ul className="ml-1 mt-8 flex items-center">
               <li className="mr-5 text-xs">
-                <a className='block hover:text-slate-200' href='#'>
+                <a className='block hover:text-slate-200' href='https://github.com/disinibale' target='_blank'>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-6 w-6" aria-hidden="true"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
                 </a>
               </li>
               <li className="mr-5 text-xs">
-                <a className='block hover:text-slate-200' href='#'>
+                <a className='block hover:text-slate-200' href='https://www.linkedin.com/in/disinibale' target='_blank'>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6" aria-hidden="true"><path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path></svg>
                 </a>
               </li>
@@ -154,7 +154,7 @@ function App() {
                       <div className='z-10 sm:col-span-6'>
                         <h3 className='font-medium leading-snug text-slate-200'>
                           <div>
-                            <a href='https://www.andalin.com' target='_blank' className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'>
+                            <a href='https://cartenz.co.id/' target='_blank' className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'>
                               <span className='absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block'></span>
                               <span>
                                 Lead Fullstack Engineer &nbsp;·
@@ -211,7 +211,7 @@ function App() {
                       <div className='z-10 sm:col-span-6'>
                         <h3 className='font-medium leading-snug text-slate-200'>
                           <div>
-                            <a href='https://www.andalin.com' target='_blank' className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'>
+                            <a href='https://edi-indonesia.co.id' target='_blank' className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'>
                               <span className='absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block'></span>
                               <span>
                                 Fullstack Engineer &nbsp;·
@@ -280,7 +280,7 @@ function App() {
                       <div className='z-10 sm:col-span-6'>
                         <h3 className='font-medium leading-snug text-slate-200'>
                           <div>
-                            <a href='https://www.andalin.com' target='_blank' className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'>
+                            <a href='https://eva.id' target='_blank' className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'>
                               <span className='absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block'></span>
                               <span>
                                 Fullstack Developer &nbsp;·
@@ -293,7 +293,7 @@ function App() {
                           </div>
                         </h3>
                         <p className='mt-2 text-sm leading-normal'>
-                          I have had contribute to impactful projects that have transformed the customer experience. One such project involved developing a user-friendly web console system for the administration panel, empowering customers to effortlessly create chatbots and enhance their knowledge base. Additionally, I had the chance to improve the performance by meticulously addressing over 10 critical bugs in the EVA application, ensuring seamless operations and user satisfaction. As part of my responsibilities, I successfully designed and implemented a secure payment gateway, and enhancing the functionality of the web application. To ensure compatibility across web and mobile platforms.
+                          I have had contribute to impactful projects that have transformed the customer experience. One such project involved developing a user-friendly web console system for the administration panel, empowering customers to effortlessly create chatbots and enhance their knowledge base. Additionally as part of my responsibilities, I successfully designed and implemented a secure payment gateway, and enhancing the functionality of the web application. To ensure compatibility across web and mobile platforms.
                         </p>
                         <ul className='mt-2 flex flex-wrap'>
                           <li className='mr-1.5 mt-2'>
@@ -334,20 +334,19 @@ function App() {
                       <div className='z-10 sm:col-span-6'>
                         <h3 className='font-medium leading-snug text-slate-200'>
                           <div>
-                            <a href='https://www.andalin.com' target='_blank' className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'>
+                            <a className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'>
                               <span className='absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block'></span>
                               <span>
                                 Freelance &nbsp;·
                                 <span className='inline-block'>
-                                  &nbsp; Fiver | Project.id
-                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path></svg>
+                                  &nbsp; Fiverr | Project.id
                                 </span>
                               </span>
                             </a>
                           </div>
                         </h3>
                         <p className='mt-2 text-sm leading-normal'>
-                          Developing a wide range of web applications to cater to diverse user needs, including point of sale applications, marketplaces, and attendance systems. Throughout these projects, I have prioritized the creation of reusable code and libraries, enabling efficient development for future endeavors. To enhance performance, I have diligently optimized and maintained the applications, ensuring maximum speed and scalability. Furthermore, I have placed great importance on cross-platform optimization, guaranteeing a seamless and responsive experience for mobile users. My commitment to delivering high-quality solutions and prioritizing user satisfaction remains steadfast in every project I undertake.
+                          Developing a wide range of web applications to cater to diverse user needs, including point of sale applications, marketplaces, and attendance systems. Throughout these projects, I have prioritized the creation of reusable code and libraries, enabling efficient development for future use. To enhance performance, I have diligently optimized and maintained the applications, ensuring maximum speed and scalability. Furthermore, I have placed great importance on cross-platform optimization, guaranteeing a great and responsive experience for mobile users.
                         </p>
                         <ul className='mt-2 flex flex-wrap'>
                           <li className='mr-1.5 mt-2'>
@@ -400,20 +399,19 @@ function App() {
                       <div className='z-10 sm:col-span-6'>
                         <h3 className='font-medium leading-snug text-slate-200'>
                           <div>
-                            <a href='https://www.andalin.com' target='_blank' className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'>
+                            <a className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'>
                               <span className='absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block'></span>
                               <span>
                                 Backend Engineer (Intern) &nbsp;·
                                 <span className='inline-block'>
                                   &nbsp; Rail System
-                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path></svg>
                                 </span>
                               </span>
                             </a>
                           </div>
                         </h3>
                         <p className='mt-2 text-sm leading-normal'>
-                          Contributed to projects includes creating a server and database infrastructure for a microservice architecture using Docker. Additionally, I have leveraged PHP with the Laravel Framework to design and develop over 10 REST APIs, delivering robust and efficient solutions. Another significant accomplishment was the development of a web-based new student registration application, which has been successfully utilized by SMK TI Garuda Nusantara Cimahi for a span of three years. These achievements showcase my commitment to delivering high-quality solutions that meet the unique needs of clients, fostering seamless experiences and long-term success.
+                          Contributed to projects includes creating a server and database infrastructure for a microservice architecture using Docker. Additionally, I have leveraged PHP with the Laravel Framework to design and develop over 10 REST APIs. Another significant accomplishment was the development of a web-based new student registration application, which has been successfully utilized by SMK TI Garuda Nusantara Cimahi for a span of three years.
                         </p>
                         <ul className='mt-2 flex flex-wrap'>
                           <li className='mr-1.5 mt-2'>
@@ -434,6 +432,17 @@ function App() {
                         </ul>
                       </div>
                     </div>
+                  </li>
+                  <li className='mb-12'>
+                    <a href='#' className='inline-flex items-center font-medium leading-tight text-slate-200 font-semibold text-slate-200 group'>
+                      <span>
+                        <span className='border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none'>Download </span>
+                        <span className='whitespace-nowrap'>
+                          <span className='border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none'>Résumé</span>
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none" aria-hidden="true"><path fill-rule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clip-rule="evenodd"></path></svg>
+                        </span>
+                      </span>
+                    </a>
                   </li>
                 </ol>
               </div>
